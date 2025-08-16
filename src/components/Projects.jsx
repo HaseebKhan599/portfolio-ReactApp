b376b26b60e83a4e025e2cc3e782854b63db30f1
Abdul Haseeb Khan <abdulhaseebkhan499@gmail.com>
2025-08-16 18:05:03 +0500

    engines change in package

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,17 +5,20 @@ export default function Projects() {
     {
       title: "PhishFender",
       desc: "Chrome Extension for phishing detection using ML and Python.",
-      link: "#"
+      link: "#",
+      git: "#"
     },
     {
       title: "Portfolio Website",
       desc: "Responsive portfolio to showcase my work using React.",
-      link: "#"
+      link: "#",
+      git: "https://github.com/HaseebKhan599/portfolio-ReactApp"
     },
     {
       title: "Keeper App (Google Keep Clone)",
       desc: "A sleek React app inspired by Google Keep, letting users quickly create, view, and delete notes in an intuitive and responsive interface.",
-      link: "#"
+      link: "#",
+      git: "#"
     },
   ];
 
@@ -27,7 +30,8 @@ export default function Projects() {
           <div key={i} className="p-6 rounded-lg shadow bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700">
             <h3 className="text-xl font-semibold mb-2">{p.title}</h3>
             <p className="text-gray-600 dark:text-gray-300">{p.desc}</p>
-            <a href={p.link} className="text-blue-500 hover:underline mt-2 inline-block">View</a>
+            <a href={p.link} className="text-blue-500 hover:underline mt-2 inline-block">View</a><br/>
+            <a href={p.git} className="text-blue-500 hover:underline mt-2 inline-block">Github Repo</a>
           </div>
         ))}
       </div>
